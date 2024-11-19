c5373198fdca7069961a2c3619641b112bc23800
felipepizznato <felipepizzinato9@gmail.com>
2024-11-18 21:14:46 -0300

    Criando página home

--- gs/src/app/page.tsx
+++ gs/src/app/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+import React, { useEffect } from 'react';
+
+export default function HomePage() {
+  useEffect(() => {
+    // Executa a animação quando o componente for montado
+    const elements = document.querySelectorAll('.animated-content');
+    elements.forEach((element) => {
+      element.classList.add('visible');
+    });
+  }, []);
+
   return (
-    <div><h1>Olá mundo</h1></div>
-  )
+    <main>
+      <div className="parte1_home animated-content">
+        <h1>
+          Transforme sua <span>Energia.</span>
+        </h1>
+        <h1>
+          Transforme o <span>Futuro!</span>
+        </h1>
+        <p>
+          Em um mundo cada vez mais consciente da necessidade de preservação ambiental, o uso
+          inteligente da energia nunca foi tão importante. Nossa plataforma oferece ferramentas
+          para monitorar, otimizar e reduzir o consumo energético, proporcionando não apenas
+          economia financeira, mas também um impacto positivo no planeta. Junte-se a nós e seja
+          parte da transformação energética!
+        </p>
+      </div>
+
+      <div className="parte2_home animated-content">
+        <div>
+          <h1>Dê o Primeiro Passo para um Futuro Energético Sustentável</h1>
+          <p>
+            Descubra como pequenas mudanças podem fazer uma grande diferença. Cadastre-se,
+            monitore seu consumo e receba sugestões personalizadas para otimizar sua utilização
+            de energia. O futuro começa com a sua ação – e juntos, podemos construir um amanhã
+            mais sustentável.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
